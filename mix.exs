b8477936e6a490af6be3cc17d58f08110660e6c1
camshaft <bytheway.cameron@gmail.com>
2015-06-23 14:18:29 -0600

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CountBuffer.Mixfile do
 
   def project do
     [app: :count_buffer,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
