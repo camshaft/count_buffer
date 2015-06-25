0bfd3e7fae7f3ff42400e50e37b4007f8d3163a4
camshaft <bytheway.cameron@gmail.com>
2015-06-25 00:31:29 -0600

    print queue metrics

--- lib/count_buffer/worker.ex
+++ lib/count_buffer/worker.ex
@@ -2,11 +2,19 @@ defmodule CountBuffer.Worker do
   require Logger
 
   def init(flush) do
+    :erlang.send_after(30_000, self(), :stats)
     loop(flush)
   end
 
   def loop(flush) do
     receive do
+      :stats ->
+        {_, size} = :erlang.process_info(self(), :message_queue_len)
+        if size > 0 do
+          :io.format("at=count_buffer measure#count_buffer.worker_queue=~p~n", [size])
+        end
+        :erlang.send_after(30_000, self(), :stats)
+        __MODULE__.loop(flush)
       {bucket, key, count} ->
         count = gather(bucket, key, count)
         try do
